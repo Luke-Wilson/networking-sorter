213e2c6cec4c490e8bc7d5b9a6ef5651ccc9a6ca
Luke Wilson <wilson.palooza@gmail.com>
2016-10-17 12:47:19 -0700

    - Requires body-parser - Adds POST method for /attendee route

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var db = require('./app/config');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.listen(3003)
 
 app.use(express.static(__dirname + '/public'))
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function(req, res) {
   console.log('got something');
   res.send();
+})
+
+app.post('/attendee', function(req, res) {
+  console.log(req.body.firstname);
+  res.redirect('/');
 })
